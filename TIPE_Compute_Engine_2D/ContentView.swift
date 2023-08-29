34d63f6c69d516159f6118b2a6b7f8e0e069edd8
RRx03 <romistargames@gmail.com>
2023-08-29 11:19:31 +0200

    Initial Commit

---
+++ TIPE_Compute_Engine_2D/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TIPE_Compute_Engine_2D
+//
+//  Created by Roman Roux on 29/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TIPE_Compute_Engine_2D/TIPE_Compute_Engine_2DApp.swift
@@ -0,0 +1,17 @@
+//
+//  TIPE_Compute_Engine_2DApp.swift
+//  TIPE_Compute_Engine_2D
+//
+//  Created by Roman Roux on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TIPE_Compute_Engine_2DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
